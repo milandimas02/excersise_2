5b7bcfe195889f1860f4e03131153a63eb79fb1a
Dimas Saputra <47784932+milandimas02@users.noreply.github.com>
2019-02-22 09:25:18 +0700

    Add files via upload

---
+++ versi 1/hitung.js
@@ -0,0 +1,26 @@
+ function tambah(){ 
+      a=eval(document.getElementById("a").value);
+      b=eval(document.getElementById("b").value);
+      c=a+b;
+      document.getElementById("hasil").value=c; 
+       }
+
+function kurang(){ 
+      a=document.getElementById("a").value;
+      b=document.getElementById("b").value;
+      c=a-b;
+      document.getElementById("hasil").value=c; 
+       }
+
+function kali(){ 
+      a=document.getElementById("a").value;
+      b=document.getElementById("b").value;
+      c=a*b;
+      document.getElementById("hasil").value=c; 
+       }
+function bagi(){ 
+      a=document.getElementById("a").value;
+      b=document.getElementById("b").value;
+      c=a/b;
+      document.getElementById("hasil").value=c; 
+       }
